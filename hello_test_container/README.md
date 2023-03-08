4f747e71705da6f9a08ae01f632a5e6f700d2d31
Eddy <eddy15201@gmail.com>
2023-03-08 17:23:42 +0800

    update

--- hello_test_container/README.md
+++ hello_test_container/README.md
@@ -1,6 +1,26 @@
+# Test Container(Python + SqlAlchemy + Postgres)
 
+## 遇到的問題
+當我們使用TDD的方式來開發時，時常會把資料庫相關的地方都直接Mock掉，來假設跟DB之間的交流是符合邏輯的。但往往會發生不如預期的事情，假如某張table要增加一個欄位，但某些地方卻沒修改到，導致服務整個爆炸，就GG了
+
+## 解決方式
+因為會有上面的問題，所以就有了`Test Container`，來支援撰寫測試案例的時候，可以幫忙起個容器(Container)，並架設指定的資料庫(Postgres、MySQL...)等等可以放在Docker上面的images
+
+## 實作分享
+1. 安裝套件
 ```
 pip install testcontainers
 pip install SQLAlchemy
 pip install psycopg2-binary
 ```
+2. 定義model(DB table)
+3. 撰寫db_mask
+
+
+## 總結
+
+
+
+## 參考
+1. https://nijialin.com/2021/11/25/python-testcontainer-fasstapi-database/
+
--- hello_test_container/hello.py
+++ hello_test_container/hello.py
@@ -1,14 +1,31 @@
+import unittest
 from dataclasses import dataclass
 
 import psycopg2
 import sqlalchemy
-from sqlalchemy import text
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import text, Column, String
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
 from testcontainers.postgres import PostgresContainer
 
-from hello_test_container.crud import create_user
-
 Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    uid = Column(String(100), primary_key=True, unique=True, index=True)
+    name = Column(String(100))
+    picture = Column(String(255))
+
+    def __str__(self):
+        return f"User uid: {self.uid}, name: {self.name}, picture: {self.picture}"
+
+
+def create_user(db: Session, user):
+    db_user = User(uid=user.uid, name=user.name, picture=user.picture)
+    db.add(db_user)
+    db.commit()
+    return db_user
 
 
 @dataclass
@@ -33,16 +50,18 @@ def db_mask(postgres):
     return override_get_db()
 
 
-with PostgresContainer("postgres:9.5") as postgres:
-    db = next(db_mask(postgres))
-    user = create_user(db, UserSchema(uid="1", name="eddy", picture="https://xx"))
-    print(user)
+class TestUserCRUD(unittest.TestCase):
+    def test_create_user(self):
+        with PostgresContainer("postgres:9.5") as postgres:
+            db = next(db_mask(postgres))
+            user = create_user(db, UserSchema(uid="1", name="eddy", picture="https://xx"))
 
-    # engine = sqlalchemy.create_engine(postgres.get_connection_url())
-    # test_session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-    # db = test_session_local()
-    # rs = db.execute(text("select version()"))
-    # for row in rs:
-    #     print(row)
+            self.assertEqual(user.uid, "1")
+            self.assertEqual(user.name, "eddy")
+            self.assertEqual(user.picture, "https://xx")
+            print(user)
+            self.assertEqual(user.uid, "2")
 
 
+if __name__ == '__main__':
+    unittest.main()
